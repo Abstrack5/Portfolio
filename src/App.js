d3a92fe691587cac42893277ad0a875b0951f242
Abstrack5 <armon_ah05@outlook.com>
2022-08-28 01:04:12 -0400

    added particles npm to homepage only, need footer, contemplating if i was typewriter effect on home page

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
-import './App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-import Header from'./Components/Header';
+import Navigation from "./Components/Navigation";
 
-
-import Homepage from './Pages/Homepage/Homepage';
-import About from './Pages/About/About';
-import Project from './Pages/Project/Project';
-import Contact from './Pages/Contact/Contact';
+import Homepage from "./Pages/Homepage/Homepage";
+import About from "./Pages/About/About";
+import Project from "./Pages/Project/Project";
+import Contact from "./Pages/Contact/Contact";
 
 function App() {
   return (
     <>
-    <Router>
-      <Header />
-      <Routes>
-        <Route path='/' element={<Homepage />} />
-        <Route path='/about' element={<About />} />
-        <Route path='/projects' element={<Project />} />
-        <Route path='/contact' element={<Contact />} />
-      </Routes>
-    </Router>
+      <Router>
+        <Navigation />
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/projects" element={<Project />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </Router>
     </>
   );
 }
--- src/Pages/Homepage/Homepage.js
+++ src/Pages/Homepage/Homepage.js
@@ -1,11 +1,13 @@
 import React from "react";
+import Particles from "../../Components/Particles";
 
 function Homepage() {
-    return (
-        <>
-        <h1 className="text-center">Homepage</h1>
-        </>
-    )
+  return (
+    <>
+      <Particles id="ts-particles" />
+      <h1 className="text-center">Homepage</h1>
+    </>
+  );
 }
 
 export default Homepage;
