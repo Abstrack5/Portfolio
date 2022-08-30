f060861bb88dd8726534c5ade205c3ca79508202
Abstrack <Armon_ah05@outlook.com>
2022-08-30 18:50:30 -0400

    added about me section, img and description, need to find things to exxpress about me

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -1,11 +1,46 @@
 import React from "react";
+import "./About.css";
+import HeadshotPic from "../../images/headshot.jpg";
+
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 function About() {
-    return (
-        <>
-        <h1 className="text-center">About Me</h1>
-        </>
-    )
+  return (
+    <div className="container">
+      <h2 className="AboutMeHeader">About Me</h2>
+      <div className="AboutMeContainer row">
+        <div className="row">
+          <div className="ImgContainer col-sm-auto col-md col-lg">
+            <img
+              src={HeadshotPic}
+              alt="headshot"
+              className="ImgBox img-fluid"
+            />
+          </div>
+          <div className="AboutMeBox col-sm-auto col-md-auto col-lg">
+            <div className="AboutMeContentBox">
+              <p className="AboutMeContent pt-4">
+                Hello! My name is Armon Ahmadi and I am excited you are here!
+              </p>
+              <p className="AboutMeContent">
+                lorem ipsum dolor lorem ipsum lorem lorem ipsum lorem ipsum
+                lorem ipsum dolor lorem ipsum lorem lorem ipsum lorem ipsum
+                lorem ipsum dolor lorem ipsum lorem lorem ipsum lorem ipsum
+                lorem ipsum dolor lorem ipsum lorem lorem ipsum lorem ipsum
+              </p>
+              <p className="AboutMeContent">
+                lorem ipsum dolor lorem ipsum lorem lorem ipsum lorem ipsum
+                lorem ipsum dolor lorem ipsum lorem lorem ipsum lorem ipsum
+                lorem ipsum
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default About;
