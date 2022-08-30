2570b6c536dd9ff2a829f9bad752905fc8ad8483
Abstrack <Armon_ah05@outlook.com>
2022-08-30 19:43:45 -0400

    added project photos, started working on helper to map data in project section. Still need to finish helper data. Card bootstrap isnt being friendly and sizing correctly

--- src/Pages/Project/Project.js
+++ src/Pages/Project/Project.js
@@ -1,11 +1,35 @@
 import React from "react";
+import './Project.css';
+
+import Card from "react-bootstrap/Card";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
 
 function Project() {
-    return (
-        <>
-        <h1 className="text-center">Projects</h1>
-        </>
-    )
+  return (
+    <>
+      <h1 className="text-center">Projects</h1>
+      <div className="d-flex justify-content-center">
+        <Row xs={1} md={3} lg={2} className="g-4">
+          {Array.from({ length: 6 }).map((_, idx) => (
+            <Col>
+              <Card className="card">
+                <Card.Img variant="top" src="holder.js/100px160" />
+                <Card.Body>
+                  <Card.Title>Card title</Card.Title>
+                  <Card.Text>
+                    This is a longer card with supporting text below as a
+                    natural lead-in to additional content. This content is a
+                    little bit longer.
+                  </Card.Text>
+                </Card.Body>
+              </Card>
+            </Col>
+          ))}
+        </Row>
+      </div>
+    </>
+  );
 }
 
 export default Project;
--- src/utils/projectData.js
+++ src/utils/projectData.js
@@ -1,13 +1,14 @@
 // import images per project here
+import CoffeeBr from '../../src/images/coffe-br.png';
 
 
 export const projectData = [
     {
-        title:'',
-        image: '',
-        description:'',
-        website: '',
-        github:''
+        title:'Coffee <br>',
+        image: {CoffeeBr},
+        description:'A user friendly app designed for coffee shops that need to manage that morning rush of caffeine-crazed orders.',
+        website: 'https://limitless-everglades-24758.herokuapp.com/',
+        github:'https://github.com/Abstrack5/Coffee-br'
     },
     {
         title:'',
